e53eff9daef99d77336bdee33094e694e36d447c
IreneMercy <irenemercy700@gmail.com>
2019-11-20 16:38:42 +0300

    add secret key

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -14,6 +14,9 @@ import os
 import dj_database_url
 from decouple import config, Csv
 import django_heroku
+
+SECRET_KEY = config('SECRET_KEY')
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
